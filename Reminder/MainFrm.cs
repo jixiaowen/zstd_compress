b342e42b2a859435d8ba996bd2ccf64da1b0ef22
Ji <jixiaowen@live.com>
2026-01-14 17:01:04 +0800

    'init'

---
+++ Reminder/MainFrm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Reminder
+{
+    public partial class MainFrm : Form
+    {
+        WorkFrm wrkFrm;
+        public MainFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainFrm_Load(object sender, EventArgs e)
+        {
+
+        }
+       
+
+        private void Btn_start_Click(object sender, EventArgs e)
+        {
+            bool input_flag;
+
+            if (this.ckBoxInput.Checked)
+            {
+                input_flag = true;
+            }
+            else {
+                input_flag = false;
+            }
+
+            int wrkTime = (int)this.numWrkTime.Value;
+            int rstTime = (int)this.numRstTime.Value;
+            
+            if (wrkFrm != null && !wrkFrm.IsDisposed)
+            {
+                wrkFrm.StopTimer();
+                wrkFrm.Close();
+            }
+            
+            wrkFrm = new WorkFrm(wrkTime,rstTime,input_flag);
+            wrkFrm.Show();
+            this.Visible = false;
+
+        }
+
+        private void 主窗体ToolStripMenuItem_Click(object sender, EventArgs e)
+        {            
+            this.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+            if (wrkFrm!=null)
+            {
+                wrkFrm.Close();
+            }
+            
+
+        }
+
+        private void MainFrm_FormClosing(object sender, FormClosingEventArgs e)
+        {            
+            e.Cancel = true;
+            this.WindowState = FormWindowState.Minimized;
+            this.Visible = false;
+            this.ShowInTaskbar = false;
+            notifyIcon1.Visible = true;
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            System.Environment.Exit(0);
+        }
+
+        private void 关于ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox aboutBox = new AboutBox();
+            aboutBox.ShowDialog();
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+        }
+    }
+}
